b18aa0a744ac266836d71ccad62206359de524bb
a.kluev <a.kluev@php73.ru>
2019-08-20 18:33:09 +0400

    ENV for ui

--- ui/app.ts
+++ ui/app.ts
@@ -1,10 +1,10 @@
 import { Api } from "./api";
 import { Renderer } from "./renderer";
 import { Ui } from "./ui";
+import * as device from "../device.json";
 
 (function main() {
-  // const HOST = document.location.hostname === 'localhost' ? '192.168.1.58' : document.location.hostname;
-  const HOST = document.location.hostname === 'localhost' ? '192.168.252.245' : document.location.hostname;
+  const HOST = ['127.0.0.1', 'localhost'].indexOf(document.location.hostname) > -1 ? device.ip : document.location.hostname;
   const api = new Api(HOST);
 
   const renderer = new Renderer();
